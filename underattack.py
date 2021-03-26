48f77ee6ecc7937e10bbdd11e0cc9fd2c4c8e6b2
godares79 <david.drich@gmail.com>
2021-03-25 23:33:26 -0400

    comment

--- underattack.py
+++ underattack.py
@@ -1,3 +1,4 @@
 # Utility function to check if currently under attack.
 # Should be checked after my current target is killed, to check if I am currently
 # under attack by anything.
+# See if it is something that can be handled automatically and if not play an alert sound.
