59deed75184430aae136edd03097cc2b1db9ca2b
godares79 <david.drich@gmail.com>
2021-03-28 17:07:51 -0400

    Add mana monitoring to resourcemonitor.

--- resourcemonitor.py
+++ resourcemonitor.py
@@ -16,25 +16,31 @@ class ResourceMonitorThread(Thread):
     self.has_low_health = None
     self.has_low_mana = None
     self.has_high_health = None
+    self.has_high_mana = None
 
   def run(self):
     while True:
       current_health_percent = self.screen_monitor.get_screen_object().get_health_percent()
-      if current_health_percent < 25:
+      if current_health_percent < 20:
         self.has_low_health = True
       else:
         self.has_low_health = False
 
-      if current_health_percent > 85:
+      if current_health_percent > 80:
         self.has_high_health = True
       else:
         self.has_high_health = False
 
-      # Not bothering to track Mana currently.
-      # if self.screen_monitor.get_screen_object().get_mana_percent() < 30:
-      #   self.has_low_mana = True
-      # else:
-      #   self.has_low_mana = False
+      current_mana_percent = self.screen_monitor.get_screen_object().get_mana_percent()
+      if current_mana_percent < 10:
+        self.has_low_mana = True
+      else:
+        self.has_low_mana = False
+
+      if current_mana_percent > 80:
+        self.has_high_mana = True
+      else:
+        self.has_high_mana = False
 
       # If using a pet then monitor the pet health and alert if it starts getting attacked.
       if self.using_pet:
